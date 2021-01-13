11a873a2092e074869a70ee52d48c4b6fdaa942c
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-13 16:25:37 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,10 +1,9 @@
-require_relative "../config/environment.rb"
+require_relative '../config/environment'
 require 'active_support/inflector'
 
 class InteractiveRecord
-
   def self.table_name
-    self.to_s.downcase.pluralize
+    to_s.downcase.pluralize
   end
 
   def self.column_names
@@ -15,14 +14,14 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      column_names << row["name"]
+      column_names << row['name']
     end
     column_names.compact
   end
 
-  def initialize(options={})
+  def initialize(options = {})
     options.each do |property, value|
-      self.send("#{property}=", value)
+      send("#{property}=", value)
     end
   end
 
@@ -41,16 +40,15 @@ class InteractiveRecord
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
-    values.join(", ")
+    values.join(', ')
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    self.class.column_names.delete_if { |col| col == 'id' }.join(', ')
   end
 
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, name)
-end
-
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
 end
